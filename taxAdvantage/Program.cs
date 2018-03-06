59774a79a916d919f1fb6ef0066c8d1879fe993f
ftwdjw <ftwdjw@gmail.com>
2018-03-06 05:29:22 -0600

    works

--- taxAdvantage/Program.cs
+++ taxAdvantage/Program.cs
@@ -8,8 +8,9 @@ namespace taxAdvantage
         public static void Main(string[] args)
         {
             int i, maxYears = 34;
-            double investment = 1.0;
-            double investmentTaxable = 1.0;
+            //assume $1 for both investments
+            //double investment = 1.0;
+            //double investmentTaxable = 1.0;
             double taxRate = 0.20;
             double returnRate = .06;
             double taxableReturnRate = returnRate*(1 - taxRate);
@@ -19,18 +20,19 @@ namespace taxAdvantage
 
             for (i = 0; i < maxYears; i++)
             {
+                
+                exemptAccount = Math.Pow(1 + returnRate, i+1);
+                nonExempt = Math.Pow(1 + taxableReturnRate, i + 1);
+                gain = (exemptAccount - nonExempt)/nonExempt;
+                outputFile[i] = string.Format("{0:#},{1:C},{2:C},{3:P}", 
+                                              i+1, exemptAccount,nonExempt, gain);
 
-                exemptAccount = investment * (1 + returnRate);
-                nonExempt = investmentTaxable * (1 + taxableReturnRate);
-                gain = (exemptAccount - nonExempt);
-                outputFile[i] = string.Format("{0:#},{1:C},{2:C},{3:P}", i+1, exemptAccount,nonExempt, gain);
-
-                Console.WriteLine("year = {0:#} exempt Account={1:C}", i+1, exemptAccount);
-                Console.WriteLine("year = {0:#} non exempt Account={1:C}", i+1, nonExempt);
+                Console.WriteLine("year = {0:#} exempt Account={1:C}", 
+                                  i+1, exemptAccount);
+                Console.WriteLine("year = {0:#} non exempt Account={1:C}",
+                                  i+1, nonExempt);
                 Console.WriteLine("year = {0:#} gain={1:P}", i+1, gain);
 
-                investment = exemptAccount;
-                investmentTaxable = nonExempt;
             }
 
             File.WriteAllLines("gain.csv",outputFile);
